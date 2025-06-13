83c5a4748027a80cbbc2401f4454a1d9bc7ac37e
Ubalasiya <skylifecommunity0@gmail.com>
2025-06-13 16:18:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "RASHU-MD=F8ZX0SYY#sU8863QhvH5SBjrcywPW5xJJJlPVEz_tYviN0JiJHzk",
+SESSION_ID: process.env.SESSION_ID || "RASHU-MD=CkxD3TCD#grcR1uxPkOdSuwSO_PouyayXwIQ45NT56KM-P-onESk",
 PREFIX: process.env.PREFIX || '.' ,
 MODE: process.env.MODE === undefined ?"public" : process.env.MODE,
 AUTO_VOICE: process.env.AUTO_VOICE === undefined ? "false" : process.env.AUTO_VOICE,
